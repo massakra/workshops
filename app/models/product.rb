952c8e7fa5180517bcd1dfce6d8bb8df92e57fcd
Dariusz Piwowarski <piwowarskidariusz@gmail.com>
2014-07-06 18:04:20 +0200

    Add product validators and average_ratings method

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,15 @@ class Product < ActiveRecord::Base
   belongs_to :category
   belongs_to :user
   has_many :reviews
+
+  validates_presence_of :description, :title, :price
+  validates_format_of :price, :with => /\A\d+\.*\d{0,2}\z/
+
+  def average_rating
+    rating_sum = 0
+    self.reviews.each do |r|
+      rating_sum += r.rating
+    end
+    rating_sum / self.reviews.size.to_f
+  end
 end
